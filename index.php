f070843c14f0104dbbcec6ebd2d8ba68192c7331
yungjr <65478511+yungjr@users.noreply.github.com>
2022-08-24 21:15:33 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php 
+$myLinks = array(
+	"https://yungjr.github.io/wallpapers/01.png", 
+    "https://yungjr.github.io/wallpapers/02.png",
+    "https://yungjr.github.io/wallpapers/03.jpg",
+    "https://yungjr.github.io/wallpapers/04.jpg",
+    "https://yungjr.github.io/wallpapers/05.jpg",
+    "https://yungjr.github.io/wallpapers/06.jpg",
+    "https://yungjr.github.io/wallpapers/07.png",
+    "https://yungjr.github.io/wallpapers/08.png",
+    "https://yungjr.github.io/wallpapers/09.png",
+    "https://yungjr.github.io/wallpapers/10.png",
+    "https://yungjr.github.io/wallpapers/11.jpg",
+    "https://yungjr.github.io/wallpapers/12.png",
+    "https://yungjr.github.io/wallpapers/13.jpg");
+
+$randomRedirection = $myLinks[array_rand($myLinks)];
+header("Location: $randomRedirection");
+?>
+
+</body>
+</html>
